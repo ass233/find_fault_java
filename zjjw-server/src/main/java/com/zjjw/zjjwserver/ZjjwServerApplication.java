10d543ac3555693ba84ffd09990ef0bbb7471872
jdfrozen <616769416@qq.com>
2019-09-23 19:45:10 +0800

    修改zk

--- zjjw-server/src/main/java/com/zjjw/zjjwserver/ZjjwServerApplication.java
+++ zjjw-server/src/main/java/com/zjjw/zjjwserver/ZjjwServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.net.InetAddress;
 
 @SpringBootApplication
