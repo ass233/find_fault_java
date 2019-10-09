a5b9178125e1fc0303640063dc0341d2531b4c07
jdfrozen <616769416@qq.com>
2019-10-09 18:37:07 +0800

    增加用户注册接口

---
+++ zjjw-route/src/test/java/com/zjjw/zjjwroute/system/UserTest.java
@@ -0,0 +1,21 @@
+package com.zjjw.zjjwroute.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author: Frozen
+ * @create: 2019-10-09 18:33
+ * @description: 用户测试
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserTest {
+
+	@Test
+	public void addUsers(){
+
+	}
+}
