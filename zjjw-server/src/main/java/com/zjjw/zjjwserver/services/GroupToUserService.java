ff78aa422a2f3e7a3a9e96dc670848e3f04734f3
jdfrozen <616769416@qq.com>
2019-10-10 19:53:52 +0800

    增加群发送消息

--- zjjw-server/src/main/java/com/zjjw/zjjwserver/services/GroupToUserService.java
+++ zjjw-server/src/main/java/com/zjjw/zjjwserver/services/GroupToUserService.java
@@ -4,8 +4,11 @@ import com.zjjw.zjjwserver.dao.GroupToUserMapper;
 import com.zjjw.zjjwserver.dao.UserGroupMapper;
 import com.zjjw.zjjwserver.po.GroupToUser;
 import com.zjjw.zjjwserver.po.UserGroup;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Auther: zjjw
@@ -22,4 +25,7 @@ public class GroupToUserService {
 		return groupToUserMapper.insert(groupToUser);
 	}
 
+	public List<GroupToUser> list(GroupToUser groupToUser){
+		return groupToUserMapper.list(groupToUser);
+	}
 }
